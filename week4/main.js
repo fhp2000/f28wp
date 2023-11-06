38342e3353c045534280051ecc6f7f4b83351c8c
fhp2000 <145046982+fhp2000@users.noreply.github.com>
2023-11-06 20:18:38 +0400

    Update main.js

--- week4/main.js
+++ week4/main.js
@@ -1,6 +1,12 @@
-var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://fhp2000.github.io/fhp2000.github.io/cities1.json');
-ourRequest.onload = function () {
-    console.log(ourRequest.responseText);
-};
-ourRequest.send();
+
+
+  var ourRequest = new XMLHttpRequest();
+  ourRequest.open('GET', 'https://fhp2000.github.io/fhp2000.github.io/cities1.json');
+  ourRequest.onload = function() {
+    var ourData = ourRequest.responseText
+   console.log(ourData[0]);
+  };
+  ourRequest.send();
+  
+
+
